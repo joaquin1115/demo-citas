fbf75fe2dec489a8fb58324bcf7f01b639d07597
José Joaquín Alvarado Usucachi <joaquin14142@gmail.com>
2025-05-11 21:36:50 -0500

    Fix routing syntax and import errors

--- src/modules/appointments/appointments.routes.ts
+++ src/modules/appointments/appointments.routes.ts
@@ -1,9 +1,9 @@
 import { RouteObject } from 'react-router-dom';
-import Appointments from './pages/Appointments';
+import Appointments from '../../pages/Appointments';
 
 export const appointmentsRoutes: RouteObject[] = [
   {
     path: '/appointments/*',
-    element: <Appointments />
+    element: <Appointments/>
   }
 ];
