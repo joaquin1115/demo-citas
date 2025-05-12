6e9b78c3abde4623a8e4f3217cd02b97bf321f5f
José Joaquín Alvarado Usucachi <joaquin14142@gmail.com>
2025-05-11 21:37:59 -0500

    Remove duplicate appointments routes file

--- src/modules/appointments/appointments.routes.ts
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from 'react-router-dom';
-import Appointments from '../../pages/Appointments';
-
-export const appointmentsRoutes: RouteObject[] = [
-  {
-    path: '/appointments/*',
-    element: <Appointments />
-  }
-];
